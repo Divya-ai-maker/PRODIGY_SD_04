b3b57c325f1f22be2f05cbc5914992fa140acfdf
Divya-ai-maker <divyadharshini260907@gmail.com>
2026-06-12 18:51:17 +0530

    Update suduko.py

--- suduko.py
+++ suduko.py
@@ -1,66 +1,52 @@
-def find_empty(grid):
-    for row in range(9):
-        for col in range(9):
-            if grid[row][col] == 0:
-                return row, col
-    return None
-
-def check(grid, row, col, num):
-
+def check(board, row, col, num):
     for i in range(9):
-        if grid[row][i] == num:
+        if board[row][i] == num:
             return False
 
     for i in range(9):
-        if grid[i][col] == num:
+        if board[i][col] == num:
             return False
 
-    start_row = (row // 3) * 3
-    start_col = (col // 3) * 3
+    r = row - row % 3
+    c = col - col % 3
 
-    for i in range(start_row, start_row + 3):
-        for j in range(start_col, start_col + 3):
-            if grid[i][j] == num:
+    for i in range(3):
+        for j in range(3):
+            if board[r+i][c+j] == num:
                 return False
 
     return True
 
-def solve(grid):
+def solve(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if check(board, row, col, num):
+                        board[row][col] = num
 
-    empty = find_empty(grid)
+                        if solve(board):
+                            return True
 
-    if empty is None:
-        return True
+                        board[row][col] = 0
+                return False
+    return True
 
-    row, col = empty
-
-    for num in range(1, 10):
-
-        if check(grid, row, col, num):
-
-            grid[row][col] = num
-
-            if solve(grid):
-                return True
-
-            grid[row][col] = 0
-
-    return False
-
-grid = [
-    [5,3,0,0,7,0,0,0,0],
-    [6,0,0,1,9,5,0,0,0],
-    [0,9,8,0,0,0,0,6,0],
-    [8,0,0,0,6,0,0,0,3],
-    [4,0,0,8,0,3,0,0,1],
-    [7,0,0,0,2,0,0,0,6],
-    [0,6,0,0,0,0,2,8,0],
-    [0,0,0,4,1,9,0,0,5],
-    [0,0,0,0,8,0,0,7,9]
+board = [
+[5,3,0,0,7,0,0,0,0],
+[6,0,0,1,9,5,0,0,0],
+[0,9,8,0,0,0,0,6,0],
+[8,0,0,0,6,0,0,0,3],
+[4,0,0,8,0,3,0,0,1],
+[7,0,0,0,2,0,0,0,6],
+[0,6,0,0,0,0,2,8,0],
+[0,0,0,4,1,9,0,0,5],
+[0,0,0,0,8,0,0,7,9]
 ]
 
-if solve(grid):
-    for row in grid:
+if solve(board):
+    print("Solved Sudoku:")
+    for row in board:
         print(row)
 else:
     print("No Solution")
